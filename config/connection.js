a404165bf74b6ebae9392f71b417a1844859b094
HankHill <hhc3h8@gmail.com>
2022-09-28 16:54:00 -0500

    removing password lmao

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'Iraqpizza69!',
+        password: process.env.DB_PASSWORD,
         database: 'employee_db',
     },
     console.log('Connected to Employee Database.')
