15246f794844da92d131fa035dd64efa32744b56
Henry Howe <106399029+PropainAcessories@users.noreply.github.com>
2022-09-28 16:50:04 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'Iraqpizza69!',
+        password: '',
         database: 'employee_db',
     },
     console.log('Connected to Employee Database.')
